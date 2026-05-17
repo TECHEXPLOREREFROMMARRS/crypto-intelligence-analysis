2a1d4f5cee88d66f4730f052a96eb35869b7bd12
Anurag Ratna <anuragatgeu@gmail.com>
2026-05-17 23:01:43 +0530

    Reverted imports and fixed Render root

--- app/main.py
+++ app/main.py
@@ -3,18 +3,12 @@ from database.connection import engine
 from database.models import Base
 from api.routes import collector
 from api.routes import ethereum
-from api.routes import whales
-from services.scheduler import start_scheduler
+
+from app.api.routes import whales
+
+
+from app.services.scheduler import start_scheduler
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes import ml
-from api.routes import importer
-from api.routes import predict
-from api.routes import analytics
-from api.routes import risk
-from api.routes import whale_history
-from api.routes import price_history
-from api.routes import alerts
-from api.routes import tokens
 
 app = FastAPI(title="Crypto Intelligence Platform")
 
@@ -35,17 +29,45 @@ start_scheduler()
 # routes
 app.include_router(collector.router)
 app.include_router(ethereum.router)
-app.include_router(ml.router)
-app.include_router(importer.router)
-app.include_router(predict.router)
-app.include_router(analytics.router)
-app.include_router(risk.router)
-app.include_router(whale_history.router)
-app.include_router(price_history.router)
-app.include_router(alerts.router)
-app.include_router(tokens.router)
 
 
 @app.get("/")
 def home():
     return {"message": "Crypto Intelligence API Running"}
+
+from app.api.routes import ml
+
+app.include_router(ml.router)
+
+from app.api.routes import importer
+
+app.include_router(importer.router)
+
+
+from app.api.routes import predict
+
+app.include_router(predict.router)
+
+from app.api.routes import analytics
+
+app.include_router(analytics.router)
+
+from app.api.routes import risk
+
+app.include_router(risk.router)
+
+
+
+from app.api.routes import whale_history
+app.include_router(whale_history.router)
+
+from app.api.routes import price_history
+app.include_router(price_history.router)
+
+from app.api.routes import alerts
+
+app.include_router(alerts.router)
+
+from app.api.routes import tokens
+
+app.include_router(tokens.router)
